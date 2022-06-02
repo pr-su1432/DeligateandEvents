4fb154cdc1861e7ad8f90903bac9d40f8ee91bd9
pr-su1432 <103477040+pr-su1432@users.noreply.github.com>
2022-06-02 10:14:06 +0530

    Add files via upload

---
+++ MultipleDelegates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateandEvents
+{
+    public class MultipleDelegates
+    {
+        delegate void DelOp(int x, int y);
+        public void ImplementDelegate()
+        {
+            DelOp obj = Operation.Add;
+            obj += Operation.Square;
+            obj(9, 5);
+            obj(8, 3);
+            Console.ReadLine();
+        }
+        public class Operation
+        {
+            public static void Add(int a, int b)
+            {
+                Console.WriteLine("Addition is : {0}", a + b);
+            }
+            public static void Square(int a, int b)
+            {
+                Console.WriteLine("Multiple is : {0}", a * b);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace DelegateandEvents
+{
+
+    // Delegate Definition or Declaration
+    public delegate int operation(int x, int y);
+    class program
+    {
+        // Method that is passes  as an Argument
+        // It has same signature as Delegates
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate of!");
+
+            //Deligate instantiation
+            operation obj = new operation(Addition);
+
+            //output
+            Console.WriteLine("Addition is={0}",obj(56,78));
+            Console.WriteLine();
+            MultipleDelegates value = new MultipleDelegates();
+            value.ImplementDelegate();
+
+        }
+    }
+}
